477a4b26a6559cef6b7011b945dc06578bed4181
Steve Swett <sswett@thinkful.com>
2015-03-11 09:43:32 -0400

    get most behavior working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// #count is the guess number (qty) - span
+// #userGuess is the value of the current guess number - input text
+// #guessButton is the button for doing the guess - input submit
+// #guessList is the unordered list for output - ul
+// .new is the "new game" link - a
+// #feedback is the initial "Make Your Guess!" but becomes feedback - div
+
+var secretNumber = 0;
+
 
 $(document).ready(function(){
 	
@@ -12,6 +21,139 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$("#guessButton").click(function(event) {
+  		handleGuessButtonClick(event);
+  	});
+
+	$("#userGuess").keydown(function(event) {
+
+		if ( event.which == 13 )
+		{
+			handleGuessButtonClick(event);
+			event.preventDefault();
+			return false;
+		}
+
+	});
+
+
+  	$(".new").click(function() {
+  		newGame();
+  	});
+
+  	newGame();
+
 });
 
 
+function newGame()
+{
+	$("#count").html("0");
+	$("#userGuess").val("");
+	$("#guessList li").remove();
+	$("#feedback").html("Make Your Guess!");
+
+	setSecretNumber();
+}
+
+
+function setSecretNumber()
+{
+    secretNumber = Math.floor(Math.random() * 100);   // returns 0 -99
+    secretNumber += 1;
+
+    // Temporary to make sure secret number is ok:
+	$("#guessList").append("<li>Secret number: " + secretNumber + "</li>");
+
+}
+
+
+function handleGuessButtonClick(pEvent)
+{
+	var input = $("#userGuess").val();
+
+	if (!isValidInput(input))
+	{
+		$("#feedback").html("Guess must be integer 1-100");
+		return;
+	}
+
+	incrementGuessCount();
+
+	var feedback = getFeedback(parseInt(input));
+	$("#feedback").html(feedback);
+
+	$("#guessList").append("<li>" + input + " - " + feedback + "</li>");
+
+	$("#userGuess").val("");
+}
+
+
+function isValidInput(pInput)
+{
+	var num = + pInput;
+
+	if (num == NaN)
+	{
+		return false;
+	}
+
+	if (num % 1 != 0)
+	{
+		return false;
+	}
+
+	if (num < 1 || num > 100)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+function incrementGuessCount()
+{
+	var currentGuess = $("#count").html();
+
+	if (currentGuess == NaN)
+	{
+		currentGuess = 0;
+	}
+
+	currentGuess++;
+	$("#count").html(currentGuess);	
+}
+
+
+function getFeedback(pGuess)
+{
+	var diff = Math.abs(secretNumber - pGuess);
+
+	if (diff == 0)
+	{
+		return "Bingo!  You got it!";
+	}
+
+	if (diff < 10)
+	{
+		return "Very hot";
+	}
+
+	if (diff < 20)
+	{
+		return "Hot";
+	}
+
+	if (diff < 30)
+	{
+		return "Warm";
+	}
+
+	if (diff < 50)
+	{
+		return "Cold";
+	}
+
+	return "Ice cold ... burrr!";
+}
